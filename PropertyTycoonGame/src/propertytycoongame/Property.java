d4e00ab007f998f00b6906d5d0f1a8b44604b25d
ItshelloWod <1164291574@qq.com>
2020-03-23 03:55:35 +0800

    sprint2

--- PropertyTycoonGame/src/propertytycoongame/Property.java
+++ PropertyTycoonGame/src/propertytycoongame/Property.java
@@ -1,42 +1,5 @@
 package propertytycoongame;
 
-<<<<<<< HEAD
-import java.util.Stack;
-
-public class Property extends Cell{
-	private int price;
-	private Stack<House> houses = new Stack<House>();
-	private Player owner;
-	private int fee;
-	private String group;
-	
-	public Property(int price,String group) {
-		// TODO Auto-generated constructor stub
-		this.group = group;
-		this.price = price;
-	}
-	
-	public boolean buildHouse() {
-		if (!houses.isEmpty()) {
-			houses.add(new House());
-		}
-		return houses.isEmpty();
-	}
-	
-	public boolean sellHouse() {
-		houses.clear();
-		return houses.isEmpty();
-	}
-	
-	public Player getOwner() {
-		return owner;
-	}
-	
-	public int calculateFee() {
-		//wait for improvement
-		return fee;
-	}
-=======
 /**
  * Property Tycoon Game Property
  * 
