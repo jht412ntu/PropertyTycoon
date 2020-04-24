b01736d8dd41761aa7d60b956a7d453b2196eefe
Hellonmdworld <60785773+Hellonmdworld@users.noreply.github.com>
2020-04-24 20:33:46 +0100

    able add potluck jail and park now

--- PropertyTycoonGame/src/propertytycoongame/csv.java
+++ PropertyTycoonGame/src/propertytycoongame/csv.java
@@ -17,44 +17,47 @@ public class csv {
         try {
 
            Property p1;
-            HashMap<Integer, Property> the = new HashMap();
             ArrayList<String[]> csvList = new ArrayList<String[]>();
             CsvReader reader = new CsvReader(filePath,',',Charset.forName("GBK"));
             reader.readHeaders();
 
             while(reader.readRecord()){
-                csvList.add(reader.getValues()); //按行读取，并把每一行的数据添加到list集合
+                csvList.add(reader.getValues()); //
             }
             reader.close();
-            System.out.println("读取的行数："+csvList.size());
+
             for(int row=0;row<csvList.size();row++){
                p1=new Property(Integer.valueOf((csvList.get(row)[0])),csvList.get(row)[1],csvList.get(row)[2],Integer.valueOf(csvList.get(row)[3]),Integer.valueOf(csvList.get(row)[4]),Integer.valueOf(csvList.get(row)[5]),Integer.valueOf(csvList.get(row)[6]),Integer.parseInt(csvList.get(row)[7]),Integer.parseInt(csvList.get(row)[8]),Integer.parseInt(csvList.get(row)[9]));
-                the.put(row,p1);
+                CentralControl.board.theboard.put(row,p1);
                 if(csvList.get(row)[10].equals("Yes")){
                     p1.available=true;
+                }
+                if(csvList.get(row)[1] .equals( "Opportunity Knocks")){
+                   CentralControl.board.theboard.put(row,CentralControl.opportunityknockCard);
+                }
+                if(csvList.get(row)[1].equals( "Pot Luck")){
+                    CentralControl.board.theboard.put(row,CentralControl.potluckCard);
                 }
                 if(csvList.get(row)[10].equals("No")){
                     p1.available=false;
                 }
+                if(csvList.get(row)[1].equals("Jail/Just visiting")){
+                    CentralControl.board.theboard.put(row,CentralControl.jail);
+                }
+                if(csvList.get(row)[1].equals( "Free Parking")){
+                    CentralControl.board.theboard.put(row,CentralControl.park);
+                }
 
             }
-
-            Property yuuy=  the.get(2);
-            System.out.println("  ewwe "+yuuy.name);
-            System.out.println("  ewwe "+yuuy.available);
-
-
-
-
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+/*
     public static void main(String[] args) {
         String filePath = "/Users/zhenyutang/Downloads/Book1.csv";
         readCsvFile(filePath);
-    }
+    }*/
 }
 
