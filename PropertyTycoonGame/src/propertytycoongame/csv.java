a6abbd2ef31f0ee9f427d8e5eddffa762f0d96cb
Hellonmdworld <60785773+Hellonmdworld@users.noreply.github.com>
2020-04-28 21:54:11 +0100

    auto find file with name now

--- PropertyTycoonGame/src/propertytycoongame/csv.java
+++ PropertyTycoonGame/src/propertytycoongame/csv.java
@@ -1,32 +1,31 @@
 package propertytycoongame;
 
 
-import java.nio.charset.Charset;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 
-import com.csvreader.CsvReader;
-
-import java.util.HashMap;
-
-import com.csvreader.CsvReader;
-
-
 public class csv {
-    public static void readCsvFile(String filePath){
+    public static void readCsvFile(String fileName){
+        String line = "";
+        String splitBy = ",";
         try {
 
            Property p1;
             ArrayList<String[]> csvList = new ArrayList<String[]>();
-            CsvReader reader = new CsvReader(filePath,',',Charset.forName("GBK"));
-            reader.readHeaders();
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            while ((line = br.readLine()) != null)   //returns a Boolean value
+            {
+                String[] current= line.split(splitBy);
+                csvList.add(current);
 
-            while(reader.readRecord()){
-                csvList.add(reader.getValues()); //
             }
-            reader.close();
+            //delete head of csv
+            csvList.remove(0);
 
-            for(int row=0;row<csvList.size();row++){
+
+            for(int row=1;row<csvList.size();row++){
                p1=new Property(Integer.valueOf((csvList.get(row)[0])),csvList.get(row)[1],csvList.get(row)[2],Integer.valueOf(csvList.get(row)[3]),Integer.valueOf(csvList.get(row)[4]),Integer.valueOf(csvList.get(row)[5]),Integer.valueOf(csvList.get(row)[6]),Integer.parseInt(csvList.get(row)[7]),Integer.parseInt(csvList.get(row)[8]),Integer.parseInt(csvList.get(row)[9]));
                 CentralControl.board.theboard.put(row,p1);
                 if(csvList.get(row)[10].equals("Yes")){
@@ -49,15 +48,18 @@ public class csv {
                 }
 
             }
+           // System.out.println(csvList.size());
+            //System.out.println(csvList.get(0)[1]);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-/*
+
     public static void main(String[] args) {
-        String filePath = "/Users/zhenyutang/Downloads/Book1.csv";
+        String filePath = "csv_board.csv";
         readCsvFile(filePath);
-    }*/
-}
+
+
+    }}
 
