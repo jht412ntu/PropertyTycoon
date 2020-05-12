2e50707f46b494bfa5320897e180a7d17202f872
412jht <44547155+412jht@users.noreply.github.com>
2020-05-12 16:34:47 +0100

    Update Cards.java

--- PropertyTycoonGame/src/propertytycoongame/Cards.java
+++ PropertyTycoonGame/src/propertytycoongame/Cards.java
@@ -1,28 +1,41 @@
 package propertytycoongame;
 import java.util.HashMap;
 import java.util.Random;
+
+/**
+ * Cards
+ * 
+ * Class that provides basic functions that building and using the Cards.
+ * 
+ * Documented by Haotian Jiao
+ * 
+ * @author Zhenyu
+ */
 public class Cards extends Cell{
 
-    public Cards() {
-        // TODO Auto-generated constructor stub
+    protected HashMap<Integer, String> cards = new HashMap<>();
+    
+    /**
+     * Constructor for Cards.
+     * 
+     * @param position The position of the Cards
+     */
+    public Cards(int position) {
+        super(position);
     }
 
     //input a map
     //random choose a number between the size of map
     //to take a card
-    protected HashMap<Integer, String> cards = new HashMap<>();
 
-
+    /**
+     * Chooses a card randomly.
+     * 
+     * @return String - a card
+     */
     public String getCard(){
         Random random = new Random();
         int index = random.nextInt(cards.size());
         return cards.get(index);
-    }//String.valueOf(card.get(index)
-
-
-
-
-    public void move(Player player,int location){
-        player.setLocation(player.getLocation()+location);
     }
 }
