cc6d47f75e03273d4ebdaeb28d52b5333711404e
412jht <44547155+412jht@users.noreply.github.com>
2020-05-12 16:33:19 +0100

    Update BankException.java

--- PropertyTycoonGame/src/propertytycoongame/BankException.java
+++ PropertyTycoonGame/src/propertytycoongame/BankException.java
@@ -1,10 +1,20 @@
 package propertytycoongame;
 
 /**
- *
- * @author Hallton
+ * BankException
+ * .
+ * 
+ * Documented by Haotian Jiao
+ * 
+ * @author Haotian Jiao
  */
 public class BankException extends Exception{
+
+    /**
+     * Constructor for BankException.
+     * 
+     * @param message A message that need to be thrown
+     */
     public BankException(String message) {
         super(message);
     }
