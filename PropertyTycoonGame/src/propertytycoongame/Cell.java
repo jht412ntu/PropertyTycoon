5a14bed9e78e319aa693f5860b7ab2e584f3e062
412jht <44547155+412jht@users.noreply.github.com>
2020-05-12 16:35:30 +0100

    Update Cell.java

--- PropertyTycoonGame/src/propertytycoongame/Cell.java
+++ PropertyTycoonGame/src/propertytycoongame/Cell.java
@@ -3,7 +3,9 @@ package propertytycoongame;
 /**
  * Cell
  *
- * The piece of board with location
+ * The piece of board with location.
+ * 
+ * Documented by Haotian Jiao
  *
  * @author Mingfeng
  */
@@ -11,15 +13,17 @@ public class Cell {
 
     private int position;
 
-    public Cell(int position) {
-    	this.position = position ;
-    }
-    
-    public Cell() {
-    	
-    }
-    
     /**
+     * Constructor for Cell.
+     * 
+     * @param position The position of the cell
+     */
+    public Cell(int position) {
+        position = this.position;
+    }
+
+    /**
+     * Accesses and returns the position.
      * 
      * @return The position of the cell (integer)
      */
@@ -27,7 +31,8 @@ public class Cell {
         return position;
     }
 
-    /** Sets the location of the cell
+    /** 
+     * Sets the location of the cell.
      * 
      * @param location The location to be set
      */
